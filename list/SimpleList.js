4b8940593e5b657e80276b9f478334978b7cad2c
Inbar Danieli <inbar0308@gmail.com>
2021-11-09 15:56:19 +0200

    Add sideBar

--- list/SimpleList.js
+++ list/SimpleList.js
@@ -16,6 +16,28 @@ const List = document.getElementById("List");
 const input = document.getElementById("input");
 
 
+const site = document.getElementById("sidebar")
+const button = document.getElementById("OpenSB")
+
+function changing() {
+  if (site.classList.contains('close-sideB')) {
+    site.classList.remove('close-sideB')
+    site.classList.add('open-sideB')
+
+    button.classList.remove('close')
+    button.classList.add('open')
+  }
+
+  else {
+    site.classList.remove('open-sideB')
+    site.classList.add('close-sideB')
+
+    button.classList.remove('open')
+    button.classList.add('close')
+  }
+}
+
+
 
 function CreateList() {
   if (input.value === "") { return confirm('cannot add blank item') }
