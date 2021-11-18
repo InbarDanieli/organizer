396f683bb5d1021d7e2d14fc8920e04089f56049
Inbar Danieli <inbar0308@gmail.com>
2021-11-18 10:20:35 +0200

    adding drop-down feature

--- list/SimpleList.js
+++ list/SimpleList.js
@@ -1,21 +1,6 @@
-/************************************************** 
-remove the option to put "" item (or " " and so on)
-add option to delete items without the web asing you if you want to delete everytime
-
-make the not safe text look better
-**************************************************/
-
-
-//get html's elements
-const warningtext = document.getElementById("warningtext")
-const delButton = document.getElementById("buttonDel");
-const buttonAdder = document.getElementById("ButtonAdder");
-const ONOFFbutt = document.getElementById("OnOffButt");
-const List = document.getElementById("List");
-/** @type {HTMLInputElement} */
-const input = document.getElementById("input");
-
+//
 // sidebar
+//
 const site = document.getElementById("sidebar")
 const button = document.getElementById("OpenSB")
 
@@ -36,22 +21,25 @@ function changing() {
   }
 }
 
-
+//
 //ceate the list
+//
+const List = document.getElementById("List");
+/** @type {HTMLInputElement} */
+const input = document.getElementById("input");
 
-function CreateList() {
-  if (input.value === "") { return confirm('cannot add blank item') }
-  const ListItem = document.createElement('li');
-  List.appendChild(CreateItem(ListItem));
+
+function CreateList(inputInside) {
+  if (inputInside.value === "") { return confirm('cannot add blank item') }
+  List.appendChild(CreateItem(inputInside));
 }
-
-
 /**
  * 
  * @param {HTMLUListElement} list 
  * @returns 
  */
-function CreateItem(list) {
+function CreateItem(input) {
+  const list = document.createElement('li');
   const Box = document.createElement('input');
   Box.type = "checkbox";
   list.appendChild(Box);
@@ -75,11 +63,14 @@ function CreateItem(list) {
   return list;
 }
 
-
+//
 // on/off button:
+//
+const ONOFFbutt = document.getElementById("OnOffButt");
+const warningtext = document.getElementById("warningtext")
 const warning = document.createElement("p");
 
-ONOFFbutt.addEventListener('change', ()=> {
+ONOFFbutt.addEventListener('change', () => {
   if (!ONOFFbutt.checked) {
     warning.id = "warning"
     warning.innerText = "safe mode if off, there be no alarm when deleting the items"
@@ -113,9 +104,114 @@ function deleteItems() {
 
 }
 
+//
+//drop down
+//
+const inputDD = document.getElementById("inputDropDown")
+const e = document.getElementById("items");
+const Textoutput = document.getElementById("output");
+const DivEditArea = document.getElementById("EditArea");
+
+let DisAnble = true
+  function show() {
+    if (DisAnble === true) {
+    Textoutput.value = e.options[e.selectedIndex].text;
+    CreateList(Textoutput);
+    }
+  }
+
+
+//add the items to the drop-down
+function AddItem() {
+  if (inputDD.value === "") {
+    alert("cannot add blank value to your list")
+  }
+  else {
+    const item = document.createElement('option');
+    item.text = inputDD.value;
+    e.appendChild(item);
+    inputDD.value = "";
+  }
+}
+inputDD.addEventListener("keypress", (k) => {
+  if (k.key === 'Enter') {
+    AddItem()
+  }
+})
+
+
+
+function DelMultypulItems() {
+  var selected = [];
+  for (var option of e.options) {
+    if (option.selected) {
+      selected.push(option.index);
+    }
+  }
+  let Number = selected.length
+  console.log(Number);
+  for (i = Number - 1; i >= 0; i--) {
+    e.remove(selected[i]);
+  }
+}
+
+//create Delete items button
+const editButt = document.getElementById("EditButton")
+const DelButton = document.createElement("button");
+DelButton.id = "DelButton";
+DelButton.innerHTML = "delete"
+DelButton.addEventListener("click", () => {
+  if (e.selectedIndex === -1) {
+    alert("you need to select the item you want to delete")
+    console.log(e.selectedIndex);
+  }
+  else {
+    DelMultypulItems()
+  }
+})
+
+//open edit area button
+let tooltip = document.getElementById("ToolTip")
+tooltip.style.visibility = "hidden"
+var allform = document.querySelectorAll("#NonEditArea input, #NonEditArea button, #AddItemsElements input, #AddItemsElements button, #header");
+
+
+function EditItems() {
+
+  if (!editButt.classList.contains("open")) {
+    tooltip.style.visibility = "visible"
+    e.setAttribute("multiple", true);
+    e.classList.add("DropDownMult");
+    DivEditArea.appendChild(DelButton);
+    DivEditArea.classList.add("EditArea");
+    document.body.classList.add("Background");
+    editButt.classList.add("EditButt");
+    editButt.innerHTML = "X";
+   DisAnble = false;
+    for (let el of allform) { el.disabled = true; }
+  }
+  else {
+    tooltip.style.visibility = "hidden"
+    e.removeAttribute("multiple")
+    e.classList.remove("DropDownMult")
+    DivEditArea.removeChild(DelButton);
+    DivEditArea.classList.remove("EditArea")
+    document.body.classList.remove("Background");
+    editButt.classList.remove("EditButt")
+    editButt.innerHTML = "edit"
+     DisAnble = true
+    for (let el of allform) { el.disabled = false; }
+  }
+  editButt.classList.toggle("open")
+  document.getElementById("CenterArea").classList.toggle("CenterArea")
+}
+
+
 
 //by clicking a button:
-buttonAdder.addEventListener('click', (Event) => { CreateList() });
+const buttonAdder = document.getElementById("ButtonAdder");
+
+buttonAdder.addEventListener('click', (Event) => { CreateList(input) });
 
 //by press 'enter':
-input.addEventListener('keypress', (k) => { if (k.key === 'Enter') { CreateList() } });
+input.addEventListener('keypress', (k) => { if (k.key === 'Enter') { CreateList(input) } });
